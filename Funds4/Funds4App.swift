368c9673b0ded4363b32ac620c204d7c8c703bf2
Eifion Bedford <eifion@fuelledbytea.com>
2024-03-24 16:50:52 +0000

    Initial Commit

---
+++ Funds4/Funds4App.swift
@@ -0,0 +1,17 @@
+//
+//  Funds4App.swift
+//  Funds4
+//
+//  Created by Eifion Bedford on 24/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Funds4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
